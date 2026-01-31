d295830b857d833eff6f083c160bc6dcedf23421
EmmanuelNsabua <24mn261si@udbl.ac.cd>
2026-02-01 01:00:51 +0200

    mise à jour du layout et du design de l'application

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        // Remplace la police par défaut 'sans'
+        sans: ['"Plus Jakarta Sans"', 'sans-serif'],
+      },
+      spacing: {
+        '128': '32rem', // 512px
+        '144': '36rem', // 576px
+        '160': '40rem', // 640px
+        '192': '48rem', // 768px
+        '224': '56rem', // 896px
+        '256': '64rem', // 1024px
+      },
+    },
+  },
+  // ... le reste de ta config
+}
